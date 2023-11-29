57dc95d0151046f1cfa72844bb699d59300c635c
brentgaisford <brentgaisford@gmail.com>
2023-11-29 15:23:16 -0800

    Move confusion matrix function into shared_utils

--- utils.py
+++ utils.py
@@ -298,3 +298,46 @@ def decideLabelWithRotation(label_window,
     if label_num / len(label_window) < 0.8:
         new_label = Counter(label_window[portion:]).most_common(1)[0][0]
     return new_label
+
+
+def create_confusion_matrix(pred_labels,true_labels,file_name=None):
+    """
+    pred_labesl : list of integer labels
+    true_labels : list of integer labels
+    file_name : None means don't save only display
+    otherwise, give it a path to save "confusion-matrix-1.jpg"
+    """
+    
+    # class labels
+    class_labels = list(set(pred_labels))
+
+    # creates confusion matrix
+    cm = confusion_matrix(true_labels, pred_labels)
+    accuracy = cm / np.sum(cm, axis=1, keepdims=True)
+
+    
+    fig, ax = plt.subplots()
+    # im = ax.imshow(cm, cmap='Blues')
+    im = ax.imshow(accuracy, cmap='Blues', vmin = 0, vmax = 1) # Use accuracy values and set vmin/vmax
+
+    # Add accuracy values to the plot
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, f"{cm[i, j]}\n({accuracy[i, j]:.2f})", ha='center', va='center')
+
+    # Set axis labels and title
+    ax.set_xlabel('Predicted')
+    ax.set_ylabel('True')
+    ax.set_title('Confusion Matrix')
+    plt.xticks(np.arange(len(class_labels)), class_labels)
+    plt.yticks(np.arange(len(class_labels)), class_labels)
+    # Add colorbar
+    ax.figure.colorbar(im, ax=ax)
+
+    # Display the plot
+    if file_name is None:
+        plt.show()
+    else:
+        plt.savefig(file_name)
+
+    return fig
